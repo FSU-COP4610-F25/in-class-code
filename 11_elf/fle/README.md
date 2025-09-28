6c898e15ee9535341f3f6406f07f1e6b75d1b5f9
Xin Liu <xinliu1@umbc.edu>
2025-09-28 19:37:56 +0000

    1

--- 11_elf/fle/README.md
+++ 11_elf/fle/README.md
@@ -2,8 +2,21 @@
 
 FLE (Funny Little Executable) is a custom binary file format designed to support (static) linking and loading. We have developed a simple compiler, linker (leveraging `gcc/ld`), and loader for FLE. This format allows position-independent code with readable, writable, and executable memory mappings to be directly loaded and executed.
 
-
-'''
+## Demo 1
+```bash
 make
 ./minimal.fle ; echo $?
-'''
+```
+
+## Demo 2
+```bash
+./readfle foo.fle
+```
+
+## Demo 3
+```bash
+gcc -E foo.c
+gcc -S foo.c
+gcc -c foo.c
+readelf -a foo.o
+```
---
+++ 11_elf/fle/foo.s
@@ -0,0 +1,46 @@
+	.file	"foo.c"
+	.text
+	.globl	msg
+	.data
+	.align 8
+	.type	msg, @object
+	.size	msg, 14
+msg:
+	.string	"Hello World!\n"
+	.text
+	.globl	foo
+	.type	foo, @function
+foo:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	n(%rip), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	foo, .-foo
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
