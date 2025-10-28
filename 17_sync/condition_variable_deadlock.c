f470ebc699a746e17e9106a59922076cadf04a2d
Xin Liu <xinliu1@umbc.edu>
2025-10-28 14:04:15 +0000

    1

--- 17_sync/condition_variable_deadlock.c
+++ 17_sync/condition_variable_deadlock.c
@@ -1,10 +1,10 @@
 /*
  * How to run this program:
  *
- * 1. Save this code to a file named 'ex_1_producer_consumer_single_cv.c'.
+ * 1. Save this code to a file named 'single_condition_variable.c'.
  * 2. Open a terminal and navigate to the directory where the file is saved.
  * 3. Compile the program using the following command:
- *    gcc ex_3_single_condition_variable.c
+ *    gcc single_condition_variable.c
  * 
  * 4. Run the program with an argument that represents the buffer size (n), for example:
  *    ./a.out 1
